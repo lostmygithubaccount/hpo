24e8157cefced175273f0f4963fcf1f6f193cda6
lostmygithubaccount <cody.dkdc2@gmail.com>
2020-10-03 21:46:32 -0700

    saving

--- code/lightgbm/iris/train.py
+++ code/lightgbm/iris/train.py
@@ -12,6 +12,47 @@ from sklearn.metrics import log_loss, accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
+# define functions
+def preprocess_data(df):
+    X = df.drop(["species"], axis=1)
+    y = df["species"]
+
+    enc = LabelEncoder()
+    y = enc.fit_transform(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    return X_train, X_test, y_train, y_test, enc
+
+
+def train_model(params, num_boost_round, X_train, X_test, y_train, y_test):
+    t1 = time.time()
+    train_data = lgb.Dataset(X_train, label=y_train)
+    test_data = lgb.Dataset(X_test, label=y_test)
+    model = lgb.train(
+        params,
+        train_data,
+        num_boost_round=num_boost_round,
+        valid_sets=[test_data],
+        valid_names=["test"],
+    )
+    t2 = time.time()
+
+    return model, t2 - t1
+
+
+def evaluate_model(model, X_test, y_test):
+    y_proba = model.predict(X_test)
+    y_pred = y_proba.argmax(axis=1)
+    loss = log_loss(y_test, y_proba)
+    acc = accuracy_score(y_test, y_pred)
+
+    return loss, acc
+
+
+# start mlflow run
 with mlflow.start_run():
 
     # enable auto logging
@@ -31,6 +72,7 @@ with mlflow.start_run():
     parser.add_argument("--verbose", type=int, default=0)
     args = parser.parse_args()
 
+    # setup parameters
     num_boost_round = args.num_boost_round
 
     params = {
@@ -45,43 +87,6 @@ with mlflow.start_run():
         "seed": args.seed,
         "verbose": args.verbose,
     }
-
-    # define functions
-    def preprocess_data(df):
-        X = df.drop(["species"], axis=1)
-        y = df["species"]
-
-        enc = LabelEncoder()
-        y = enc.fit_transform(y)
-
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42
-        )
-
-        return X_train, X_test, y_train, y_test, enc
-
-    def train_model(params, num_boost_round, X_train, X_test, y_train, y_test):
-        t1 = time.time()
-        train_data = lgb.Dataset(X_train, label=y_train)
-        test_data = lgb.Dataset(X_test, label=y_test)
-        model = lgb.train(
-            params,
-            train_data,
-            num_boost_round=num_boost_round,
-            valid_sets=[test_data],
-            valid_names=["test"],
-        )
-        t2 = time.time()
-
-        return model, t2 - t1
-
-    def evaluate_model(model, X_test, y_test):
-        y_proba = model.predict(X_test)
-        y_pred = y_proba.argmax(axis=1)
-        loss = log_loss(y_test, y_proba)
-        acc = accuracy_score(y_test, y_pred)
-
-        return loss, acc
 
     print("*" * 60)
     print("\n\n")
